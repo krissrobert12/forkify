36da918fb69fa6b3e5ba3af73a991e8def4af88f
Kriss Gardner <krissrobert12@gmail.com>
2020-07-25 00:31:39 +0300

    added functionality to the search bar and basic functionality to the recipe list

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,39 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, elementStrings, renderLoader, clearLoader } from './views/base';
+
+/** Global state of the app
+ * - Search object
+ * - Current object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+
+    // 1. get query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2. new search object and add to state
+        state.search = new Search(query);
+
+        // 3. prepare ui for results
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        // 4. search for recipes
+        await state.search.getResults();
+
+        // 5. render results on ui
+        clearLoader();
+        searchView.renderResults(state.search.result);
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
